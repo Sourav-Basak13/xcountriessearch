9429247215dffe639d196c3c99714f3802cfae9c
Sourav Basak <sourav.new2016@gmail.com>
2025-07-10 06:06:11 +0530

    completed xcountriessearch

--- src/components/CountryCard/CountryCard.jsx
+++ src/components/CountryCard/CountryCard.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import styles from "./CountryCard.module.css";
+import "./CountryCard.css";
 
 function CountryCard({ common, png }) {
   return (
-    <div className={styles.countryCard}>
+    <div className="countryCard">
       <img src={png} alt={common} width={50} height={50} />
       <p>{common}</p>
     </div>
